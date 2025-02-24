7f9c17c9d15ede56a9dd06aedf03482719b5e494
Akmtkaw <tkaw-sa-snnzbldm@outlook.jp>
2025-02-24 23:15:34 +0900

    fix: for next.js 15

--- app/news/category/[id]/page.tsx
+++ app/news/category/[id]/page.tsx
@@ -6,13 +6,12 @@ import Category from "@/app/_components/Category";
 import { NEWS_LIST_LIMIT } from "@/app/_constants";
 
 type Props = {
-  params: {
-    id: string;
-  };
+  params: Promise<{ id: string }>;
 };
 
 export default async function Page({ params }: Props) {
-  const category = await getCategoryDetail(params.id).catch(notFound);
+  const { id } = await params;
+  const category = await getCategoryDetail(id).catch(notFound);
 
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT,
